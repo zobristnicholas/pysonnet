33607dcbe22cf82aa9fb7c2868aecdc30406ad11
Nicholas Zobrist <nzobrist@physics.ucsb.edu>
2018-11-19 18:27:42 -0800

    Brought main classes up to the main level in the __init__ file

---
+++ pysonnet/__init__.py
@@ -0,0 +1,6 @@
+# bring the project classes up to the main level
+from pysonnet.simulation import GeometryProject
+from pysonnet.simulation import NetlistProject
+# bring the output interface classes up to the main level
+from pysonnet.outputs import Sweep
+from pysonnet.outputs import CurrentDensity
