9e9f3d88ec1ec8caca5aa5ed21dd0b65dc218147
Nicholas Zobrist <nzobrist@physics.ucsb.edu>
2018-11-28 13:47:58 -0800

    No longer pulling NetlistProject and Sweep up to the top level since they aren't finished

--- pysonnet/__init__.py
+++ pysonnet/__init__.py
@@ -1,6 +1,6 @@
 # bring the project classes up to the main level
 from pysonnet.projects import GeometryProject
-from pysonnet.projects import NetlistProject
+# from pysonnet.projects import NetlistProject
 # bring the output interface classes up to the main level
-from pysonnet.outputs import Sweep
+# from pysonnet.outputs import Sweep
 from pysonnet.outputs import CurrentDensity
